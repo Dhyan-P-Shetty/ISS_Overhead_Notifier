c3cee29a6a41b479437bccb88e2970aadb793805
Dhyan-P-Shetty <110186365+Dhyan-P-Shetty@users.noreply.github.com>
2023-02-22 18:37:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+my_email = "dhyan2002shetty@gmail.com"
+password = "wdlzqewblvkqkiin"
+
+MY_LAT = 12.869910
+MY_LONG = 74.843918
+
+
+def is_iss_above_me():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+
+    if time_now.hour >= sunset or time_now.hour <= sunrise:
+        return True
+
+
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+while True:
+    time.sleep(60)
+    if is_iss_above_me() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=my_email,
+                                msg="Subject:Look up\n\nThe ISS is above you in the sky."
+                                )
